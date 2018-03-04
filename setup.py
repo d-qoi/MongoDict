8b5170efbce525d65d387f2d39193a17743b5918
Alex Hirschfeld <alex@d-qoi.com>
2018-03-04 17:34:28 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
-2 
-3 setup(
-4     name='mongodict', 
-5     version='0.1',
-6     scripts=['mongodict']
-7 )
+ 
+setup(
+    name='mongodict', 
+    version='0.1',
+    scripts=['mongodict']
+)
