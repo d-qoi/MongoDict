ffd2409a8956d76a530d3eb96a8fc25a7b63fe4c
Alex Hirschfeld <alex@d-qoi.com>
2018-03-04 17:45:20 -0500

    Moving things around, trying to make installable via pip

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup
 setup(
     name='mongodict', 
     version='0.1',
-    scripts=['mongodict']
+    packages=['mongodict'],
+    url='https://github.com/d-qoi/MongoDict.git',
+    author='Alex Hirschfeld',
+    author_email='alex@d-qoi.com',
+    license='MIT',
 )
