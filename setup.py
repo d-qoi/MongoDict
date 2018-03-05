31f3d662f729b5f16483b9872ef92b204cfd36d6
Alex Hirschfeld <alex@d-qoi.com>
2018-03-05 09:05:29 -0500

    slighty updates

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup
  
+
+def requirements():
+    """Build the requirements list for this project"""
+    requirements_list = []
+
+    with open('requirements.txt') as requirements:
+        for install in requirements:
+            requirements_list.append(install.strip())
+
+    return requirements_list
+
 setup(
-    name='mongodict', 
+    name='MongoDict', 
     version='0.1',
-    packages=['mongodict'],
+    packages=['MongoDict'],
     url='https://github.com/d-qoi/MongoDict.git',
     author='Alex Hirschfeld',
     author_email='alex@d-qoi.com',
     license='MIT',
+    install_requires=requirements()
 )
